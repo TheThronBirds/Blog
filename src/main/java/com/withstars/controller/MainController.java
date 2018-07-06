6d585cce92a29090b1b6bdc476441f9ffdc46d52
luzejin <luzejin@yhfin.com>
2018-07-06 13:49:27 +0800

    增加验证码

--- src/main/java/com/withstars/controller/MainController.java
+++ src/main/java/com/withstars/controller/MainController.java
@@ -5,12 +5,21 @@ import com.withstars.service.impl.ReplyServiceImpl;
 import com.withstars.service.impl.TabServiceImpl;
 import com.withstars.service.impl.TopicServiceImpl;
 import com.withstars.service.impl.UserServiceImpl;
+import com.withstars.util.Captcha;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
 import javax.jws.WebParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 /**
@@ -58,6 +67,26 @@ public class MainController {
         signupPage.addObject("topicsNum",topicsNum);
         signupPage.addObject("usersNum",usersNum);
         return  signupPage;
+    }
+
+
+    /**
+     * 生成验证码
+     * @throws IOException 
+     */
+
+    @RequestMapping("/checkcode")
+    public void drawCheckcode(HttpServletResponse res, HttpSession session) throws IOException{
+        res.setContentType("image/jpg");
+        int width = 70;
+        int height = 25;
+        Captcha c = Captcha.getInstance();
+        c.set(width, height);
+        String checkcode = c.generateCheckcode();
+        // 验证码放入session
+        session.setAttribute("cCode", checkcode);
+        OutputStream os = res.getOutputStream();
+        ImageIO.write(c.generateCheckImg(checkcode), "jpg", os);
     }
 
     /**
